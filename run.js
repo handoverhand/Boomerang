8e0b97800f66e49e0b0277fb4c1f4bbc0648575c
serkoo10 <sercoo1407@icloud.com>
2022-08-07 18:25:39 +0300

    rega

--- run.js
+++ run.js
@@ -1,12 +1,12 @@
 // Основной файл.
 // Запускает игру.
 const Game = require('./src/Game');
+const insertName = require('./src/registration');
 
 // Инициализация игры с настройками.
 const game = new Game({
   trackLength: 30,
 });
-
 
 // Запуск игры.
 game.play();
--- src/registration.js
+++ src/registration.js
@@ -1,22 +1,21 @@
 const { Sequelize } = require('sequelize');
-const readline = require('readline');
+const readline = require('readline').promises;
 const { Leaderboard } = require('../db/models');
 
 async function insertName(name) {
   try {
-    const result = await Leaderboard.create({ name });
+    await Leaderboard.create({ name });
   } catch (error) {
     console.log(error.message);
   }
+
+  const greet = readline.createInterface(
+    {
+      input: process.stdin,
+      output: process.stdout,
+    },
+  );
+  const textName = await greet.question('Введи свое имя герой ');
+  console.log(`Удачи перчик ! +  ${textName}`);
 }
-const greet = readline.createInterface(
-  {
-    input: process.stdin,
-    output: process.stdout,
-  },
-  greet.question('Введи свое имя герой ', (data) => {
-    console.log(`Удачи перчик ! +  ${data}`);
-    insertName(data);
-  }),
-);
-module.exports = greet;
+module.exports = insertName;
